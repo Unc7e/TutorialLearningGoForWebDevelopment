a664daae99af8bb32151824623351a17ffe8da4b
Leon <yanglyu.leon.7@gmail.com>
2018-08-10 10:44:46 -0700

    demo: Surfing the Net

--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
+
+	"encoding/json"
 )
 
 type Page struct {
 	Name     string
 	DBStatus bool
+}
+
+type SearchResult struct {
+	Title  string
+	Author string
+	Year   string
+	ID     string
 }
 
 func main() {
@@ -32,6 +41,18 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		db.Close()
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		result := []SearchResult{
+			SearchResult{"Moby-Dick", "Herman Melville", "1851", "222222"},
+			SearchResult{"The Adventures of Huckleberry Finn", "Mark Twain", "1884", "444444"},
+			SearchResult{"The Catcher in the Rye", "JD Salinger", "1951", "333333"},
+		}
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(result); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
